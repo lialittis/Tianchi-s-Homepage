100883941212ce392b0d62382dae1e60ff006ec3
lialittis <yu-tianchi@outlook.com>
2022-06-04 00:40:56 +0200

    three-column layout and router

---
+++ src/pages/WorkPage.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+
+const WorkPage = () => {
+    return <div>WorkList</div>
+    
+}
+
+export default WorkPage;
---
+++ src/pages/WorkPageDetail.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+
+const WorkPageDetail = () => {
+    return <div>WorkList</div>
+    
+}
+
+export default WorkPageDetail;
